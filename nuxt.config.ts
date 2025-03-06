e06e9fa279bc4adc9afc9d20a0e13135357f2c42
ImmortalAI <moniki2139@yandex.ru>
2025-03-06 16:20:58 +0300

    Added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Nora from '@primeuix/themes/aura'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@primevue/nuxt-module']
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+  modules: ['@primevue/nuxt-module'],
+  primevue: {
+    options: {
+    theme: {
+      preset: Nora
+    }
+    }
+  }
 })
