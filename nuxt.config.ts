9882d247eea0488820e2815af90480f4748ecc15
ImmortalAI <moniki2139@yandex.ru>
2025-03-07 15:54:54 +0300

    Added Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,12 @@ export default defineNuxtConfig({
       tailwindcss()
     ]
   },
-  modules: ['@primevue/nuxt-module', '@nuxt/image', '@nuxt/icon'],
+  modules: [
+    '@primevue/nuxt-module',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@pinia/nuxt'
+  ],
   primevue: {
     options: {
     theme: {
