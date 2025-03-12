c2595c13c931a02d531bd73bc88925652af33707
ImmortalAI <moniki2139@yandex.ru>
2025-03-12 07:13:21 +0300

    del console.log from proxy

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -4,7 +4,6 @@ export default defineEventHandler(async (event) => {
   const proxyUrl = useRuntimeConfig().apiProxyUrl
   
   const target = joinURL(proxyUrl, event.path)
-  console.log(`Proxying request to ${target}`)
   
   return proxyRequest(event, target)
 })
